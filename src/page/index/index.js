e7838221f4a79dc90789ad0c84638167bb4736cd
czh <czh55@qq.com>
2018-06-09 12:16:44 +0800

    将首页的关键字列表删除

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -16,13 +16,8 @@ $(function() {
     $('.banner-con').html(bannerHtml);
     // 初始化banner
     var $slider     = $('.banner').unslider({
-        dots: true
-    });
-    // 前一张和后一张操作的事件绑定
-    $('.banner-con .banner-arrow').click(function(){
-        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
-        $slider.data('unslider')[forward]();
-    });
+        dots: true//圆点
+    }); 
 
 
     var listHtml    = '',
